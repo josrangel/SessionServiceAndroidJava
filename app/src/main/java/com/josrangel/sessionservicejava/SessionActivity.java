817f06473354f845c312f08b30f0d8be6fcee847
josrangel <josrangel@hotmail.com>
2021-12-06 00:31:36 -0600

    - add implement of SessionUtils for time validation for long sessions

--- app/src/main/java/com/josrangel/sessionservicejava/SessionActivity.java
+++ app/src/main/java/com/josrangel/sessionservicejava/SessionActivity.java
@@ -10,17 +10,34 @@ import android.view.MotionEvent;
 
 import com.josrangel.sessionservicejava.service.CounterTimeoutSessionService;
 import com.josrangel.sessionservicejava.utils.DialogUtils;
+import com.josrangel.sessionservicejava.utils.SessionUtils;
+
+import java.util.Calendar;
+
+/** Source:
+ * https://stackoverflow.com/questions/5645081/android-touch-event-on-screen
+ */
 
 public class SessionActivity extends AppCompatActivity {
 
     private static Context context;
+    protected Calendar lastInteractionMoment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
         SessionActivity.context = this;
+        lastInteractionMoment = SessionUtils.getTimePlusTimeSession();
         initService();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (lastInteractionMoment == null || !SessionUtils.timeInRangeSession(lastInteractionMoment)) {
+            logout();
+        }
     }
 
     private void initService() {
@@ -47,6 +64,7 @@ public class SessionActivity extends AppCompatActivity {
 
     private void rebootCounter(){
         stopService();
+        lastInteractionMoment = SessionUtils.getTimePlusTimeSession();
         initService();
     }
 
